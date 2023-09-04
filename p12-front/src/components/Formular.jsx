a91a9bbfd1120dd6f0347e20c450121a51d453a1
Lyderic <lyderic.hancelin@gmail.com>
2023-09-04 19:01:24 +0200

    last changes

--- p12-front/src/components/Formular.jsx
+++ p12-front/src/components/Formular.jsx
@@ -3,7 +3,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import React, { useState } from "react";
 
 import DatePicker from "react-datepicker";
-import Modal from "./Modal";
+import Modal from "p14-oc-modal";
 import Select from "./Select";
 import { addEmployee } from "../app/employeeStore";
 import { useDispatch } from "react-redux";
@@ -51,80 +51,73 @@ const Formular = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='firstname'>First Name</label>
+      <label htmlFor="firstname">First Name</label>
       <input
-        type='text'
-        name='firstname'
-        id='firstname'
+        type="text"
+        name="firstname"
+        id="firstname"
         value={firstname}
         onChange={(e) => setFirstName(e.target.value)}
       />
-      <label htmlFor='lastname'>Last Name</label>
+      <label htmlFor="lastname">Last Name</label>
       <input
-        type='text'
-        name='lastname'
-        id='lastname'
+        type="text"
+        name="lastname"
+        id="lastname"
         value={lastname}
         onChange={(e) => setLastName(e.target.value)}
       />
-      <label htmlFor='birthdate'>Date of Birth</label>
-      <DatePicker id='birthdate' selected={birthdate} onChange={setBirthdate} />
+      <label htmlFor="birthdate">Date of Birth</label>
+      <DatePicker id="birthdate" selected={birthdate} onChange={setBirthdate} />
 
-      <label htmlFor='startdate'>Start Date</label>
-      <DatePicker id='startdate' selected={startDate} onChange={setStartDate} />
-      {/* <input
-        type="date"
-        name="startdate"
-        id="startdate"
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      /> */}
+      <label htmlFor="startdate">Start Date</label>
+      <DatePicker id="startdate" selected={startDate} onChange={setStartDate} />
       <fieldset>
         <legend>Address</legend>
-        <label htmlFor='street'>Street</label>
+        <label htmlFor="street">Street</label>
         <input
-          type='text'
-          name='street'
-          id='street'
+          type="text"
+          name="street"
+          id="street"
           value={street}
           onChange={(e) => setStreet(e.target.value)}
         />
-        <label htmlFor='city'>City</label>
+        <label htmlFor="city">City</label>
         <input
-          type='text'
-          name='city'
-          id='city'
+          type="text"
+          name="city"
+          id="city"
           value={city}
           onChange={(e) => setCity(e.target.value)}
         />
-        <label htmlFor='state'>State</label>
+        <label htmlFor="state">State</label>
         <Select
           options={stateArray}
           value={state}
           onChange={(e) => setState(e.target.value)}
-          title='Etat'
-          id='state'
+          title="Etat"
+          id="state"
         />
-        <label htmlFor='zipcode'>Zip Code</label>
+        <label htmlFor="zipcode">Zip Code</label>
         <input
-          type='number'
-          name='zipcode'
-          id='zipcode'
+          type="number"
+          name="zipcode"
+          id="zipcode"
           value={zipcode}
           onChange={(e) => setZipcode(e.target.value)}
         />
       </fieldset>
 
-      <label htmlFor='department'>Department</label>
+      <label htmlFor="department">Department</label>
       <Select
         options={departmentArray}
-        id='department'
-        name='department'
+        id="department"
+        name="department"
         value={department}
         onChange={(e) => setDepartment(e.target.value)}
-        title='Département'
+        title="Département"
       />
-      <button type='submit'>Save</button>
+      <button type="submit">Save</button>
       <Modal
         isOpen={isOpen}
         onClose={() => {
